6c2008af6b18bfa2161ab317326bbfa46d2dd267
zaynab-20 <zatoloye@gmail.com>
2025-02-26 12:54:36 +0100

    setting up server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 7060
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(PORT, () =>{
+    console.log(`app is running on port ${PORT} `);
+    
+})
